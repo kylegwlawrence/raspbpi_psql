1728f39894ece5768f2759d215dd892a076c9b85
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2024-04-21 18:57:17 -0700

    run query_postgres with a sql statement in the command line

---
+++ app/query_postgres.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import json
+import argparse
+
+def select_query(args):
+    """Return results of a select query to a dataframe"""
+
+    password =  args.pw
+    sql =       args.sql
+
+    # read in json
+    json_file='app/server_params.json'
+    try:
+        with open(json_file) as f:
+            params = json.load(f)
+    except:
+        with open(json_file.replace('app/','')) as f:
+            params = json.load(f)
+    
+    # read in sql file if it is passed
+    if sql.endswith(".sql"):
+        try:
+            with open(sql, 'r') as f:
+                sql = f.read()
+        except:
+            with open(sql.replace('app/',''),'r') as f:
+                sql = f.read()
+
+    user = params["user"]
+    host = params["host"]
+    port = params["port"]
+    database = params["dev_db"]
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+
+    df = pd.read_sql(sql, con=engine)
+    print(df)
+    return df
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Query postgres and return a dataframe. Optionally print n rows. Wrap the sql statement in double quotes')
+    parser.add_argument('--pw', required=True, help='password for postgres')
+    parser.add_argument('--sql', required=True, help='select statement or file path to a sql file containing a select statement')
+
+    args = parser.parse_args()
+
+    select_query(args)
